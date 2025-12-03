1a571ec5a77b49ca073527437f188e008076c9a9
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-12-03 10:14:19 +0900

    Add: basic

--- jotai/src/App.tsx
+++ jotai/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAtom } from "jotai";
+import { counterAtom } from "./atoms/counterAtom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useAtom(counterAtom);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>カウント: {count}</h1>
+      <button onClick={() => setCount((c) => c + 1)}>増やす</button>
+      <button onClick={() => setCount((c) => c - 1)}>減らす</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
